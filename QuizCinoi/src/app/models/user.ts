eb1bd0dc1d03296f200b61712394a3cd6fc7a1d0
Daniel Slowik <kkasztann@gmail.com>
2019-06-01 23:15:53 +0200

    added user model

---
+++ QuizCinoi/src/app/models/user.ts
@@ -0,0 +1,25 @@
+export interface User {
+    uid: string;
+    lokalizacja: Location;
+    points: number;
+    resultsDuel: ResultDuel;
+    availablesDuel: AvailablesDuel;
+  }
+  export interface Location {
+    latitude: number;
+    longitude: number;
+  }
+  export interface ResultDuel {
+    opponentNick: string;
+    result: string;
+    stats: Stats;
+  }
+  export interface AvailablesDuel {
+    opponentUID: string;
+  }
+  export interface Stats {
+    questions: number;
+    correct: number;
+    wrong: number;
+    gainedPoints: number;
+  }

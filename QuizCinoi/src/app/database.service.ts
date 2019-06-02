bf79577ba82b9a66894154ae3ef3ff536f3e353a
Daniel Slowik <kkasztann@gmail.com>
2019-06-03 01:09:07 +0200

    Added define location for current user

--- QuizCinoi/src/app/database.service.ts
+++ QuizCinoi/src/app/database.service.ts
@@ -6,9 +6,6 @@ import { Geolocation } from "@ionic-native/geolocation/ngx";
   providedIn: "root"
 })
 export class DatabaseService {
-  lat: number;
-  long: number;
-
   constructor(public db: AngularFirestore, private geoloc: Geolocation) {}
 
   setUser(user: User) {
@@ -24,27 +21,11 @@ export class DatabaseService {
       });
   }
 
-  defineLocation() {
-    this.geoloc
-      .getCurrentPosition()
-      .then(resp => {
-        console.log(resp.coords.latitude);
-        this.lat = resp.coords.latitude;
-        console.log(resp.coords.longitude);
-        this.long = resp.coords.longitude;
-      })
-      .then(() => {
-        console.log("ok");
-      })
-      .catch(error => {
-        console.log("Error getting location", error);
-      });
-  }
-
   getAllUsers(allUsers: User[]) {
     this.db
       .collection("users")
-      .get().toPromise()
+      .get()
+      .toPromise()
       .then(snapshot => {
         snapshot.forEach(doc => {
           allUsers.push(doc.data());
@@ -54,17 +35,5 @@ export class DatabaseService {
       .catch(err => {
         console.log("Error getting documents", err);
       });
-
-    //   this.db
-    //     .collection("users")
-    //     .get()
-    //     .toPromise()
-    //     .then(function(querySnapshot) {
-    //       querySnapshot.forEach(function(doc) {
-    //         console.log(doc.id, " => ", doc.data());
-    //         allUsers.push(doc.data());
-    //       });
-    //     });
-    // }
   }
 }
--- QuizCinoi/src/app/quiz/opponent/opponent.page.ts
+++ QuizCinoi/src/app/quiz/opponent/opponent.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from "@angular/core";
 import { AngularFireAuth } from "@angular/fire/auth";
 import { DatabaseService } from "src/app/database.service";
 import { User } from "firebase";
+import { Geolocation } from "@ionic-native/geolocation/ngx";
 
 @Component({
   selector: "app-opponent",
@@ -11,20 +12,54 @@ import { User } from "firebase";
 export class OpponentPage implements OnInit {
   constructor(
     public afAuth: AngularFireAuth,
-    public database: DatabaseService
-  ) {}
+    public database: DatabaseService,
+    private geoloc: Geolocation
+  ) {
+    console.log(this.allUsers);
+  }
   allUsers = [];
+  myLat = 0;
+  myLong = 0;
 
   ngOnInit() {
     this.database.getAllUsers(this.allUsers);
+    this.defineLocation();
   }
 
   drawOpponent() {
     console.log("Losowanie");
 
-    setTimeout(() => {
-      console.log(this.allUsers);
-    }, 3000);
+    // setTimeout(() => {
+    //   console.log(this.allUsers);
+    // }, 3000);
   }
   compareLocation() {}
+
+  defineLocation() {
+    this.geoloc
+      .getCurrentPosition()
+      .then(resp => {
+        console.log(resp.coords.latitude);
+        this.myLat = resp.coords.latitude;
+        console.log(resp.coords.longitude);
+        this.myLong = resp.coords.longitude;
+      })
+      .then(() => {
+        console.log("OK");
+      })
+      .catch(error => {
+        console.log("Error getting location", error);
+      });
+  }
+
+  calculateDistance(lat1: number, lat2: number, long1: number, long2: number) {
+    let p = 0.017453292519943295; // Math.PI / 180
+    let c = Math.cos;
+    let a =
+      0.5 -
+      c((lat1 - lat2) * p) / 2 +
+      (c(lat2 * p) * c(lat1 * p) * (1 - c((long1 - long2) * p))) / 2;
+    let dis = 12742 * Math.asin(Math.sqrt(a)); // 2 * R; R = 6371 km
+    return dis;
+  }
 }
